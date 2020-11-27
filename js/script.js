bae9e9bedf2631a4cf52b1354a71aad1a185c405
MarcoB <mrc.bsllt@gmail.com>
2020-11-27 14:01:34 +0100

    completato il primo punto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 $(document).ready(
   function() {
+
+    // creo un oggetto con le seguenti proprietà: nome, cognome e età
+    var user = {
+      nome: "Marco",
+      cognome: "Busellato",
+      eta: 28
+    }
+    console.log(user);
+
+    // stampo a schermo tutte le proprietà
+    for (key in user) {
+      console.log(key, user[key]);
+      $("#user_list").append("<li>" +  key.toUpperCase() + ": " + user[key] +  "</li>");
+    }
+
   }
 );
